71de76cdbc2e5f1a147756275ff94959b997a257
mwormley008 <mwormley008@gmail.com>
2024-05-27 13:07:32 -0700

    deleted todays folder

--- text_editor.py
+++ text_editor.py
@@ -4,4 +4,4 @@ import os
 def make_todays_file():
     today_file_name = f"{date.today()}"
     if not os.path.exists(today_file_name):
-        os.makedirs(today_file_name) 
+        os.makedirs(today_file_name)
